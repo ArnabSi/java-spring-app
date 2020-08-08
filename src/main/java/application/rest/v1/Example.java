d84d1e2b3594b085cbb58f29dbf3d7420e4b210c
Arnab Si <arnab.si@cognizant.com>
2020-08-08 17:02:42 +0530

    initial check in

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -17,7 +17,7 @@ public class Example {
     public @ResponseBody ResponseEntity<String> example() {
         List<String> list = new ArrayList<>();
         //return a simple list of strings
-        list.add("Congratulations, your application is up and running");
+        list.add("Congratulations, your application is up and and running");
         return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
     }
 
